11307cd576265cfd52edac553f46a4be12531dc4
Angelina Wei Yan Wu <angelinaweiyanwu@gmail.com>
2024-02-28 20:27:57 +0100

    Update myart.js

--- myportfolio/my.art.page/myart.js
+++ myportfolio/my.art.page/myart.js
@@ -1,4 +1,12 @@
-Splitting();
-setTimeout(() => {
-    myart.querySelector(".card")(".focus");
-},1500);
+onst indexes = document.querySelectorAll(".indexes li");
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".tab-content");
+
+function reset() {
+  for (let i = 0; i < tabs.length; i++) {
+    indexes[i].style.borderColor = "transparent";
+    tabs[i].style.zIndex = 0;
+    tabs[i].classList.remove("active");
+    contents[i].classList.remove("active");
+  }
+}
